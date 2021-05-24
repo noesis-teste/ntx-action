714d370fb9b30cc3390c9c9f3de20030aef0f845
Luis Alho <luis.e.alho@noesis.pt>
2021-05-24 16:07:26 +0100

    update action

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+const https = require('https')
+
 
 try {
     // `who-to-greet` input defined in action metadata file
@@ -8,6 +10,28 @@ try {
     console.log(`CURL to ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("status", time);
+
+    const options = {
+        hostname: 'https://reqbin.com',
+        path: '/echo/get/json',
+        method: 'GET'
+      }
+
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+      
+        res.on('data', d => {
+          process.stdout.write(d)
+        })
+      })
+      
+    req.on('error', error => {
+        console.error(error)
+    })
+
+    req.end();
+
+
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
