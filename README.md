feab8c4978cea14639310649c0f49684d08e456b
Luis Alho <luis.e.alho@noesis.pt>
2021-05-24 15:09:17 +0100

    NTX github action

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ntx-action
 Testing on NTX - GitHub Action
+
+## Inputs
+
+### `ntx-url`
+
+**Required** The URL of the machine where NTX is installed. Default: `"localhost"`
+
+## Outputs
+
+### `status`
+
+The status of the test
+
+## Example usage
+
+uses: actions/ntx-action@v1.0
+with:
+  ntx-url: 'localhost:8080'
---
+++ index.js
@@ -0,0 +1,17 @@
+const core = required('@actions/core')
+const github = required('@actions/github')
+
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('ntx-url');
+    console.log(`CURL to ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("status", time);
+    
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
