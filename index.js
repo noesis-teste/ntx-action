8cab75b549bab7b5cc20f5d93389c3c2acd54193
Luis Alho <luis.e.alho@noesis.pt>
2021-05-24 16:59:18 +0100

    update

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-const https = require('https')
+const axios = require('axios');
 
 
 try {
     // `who-to-greet` input defined in action metadata file
     const ntxURL = core.getInput('ntx-url');
-    console.log(`Web request to ${ntxURL}!`);
+    console.log(`Web request to ${ntxURL}`);
     const time = (new Date()).toTimeString();
     core.setOutput("status", time);
 
-    const options = {
-        hostname: ntxURL,
-        method: 'GET'
+   
+    (async () => {
+      try {
+        const response = await axios.get('https://reqbin.com/echo/get/json')
+        console.log("ntx-url: " + response.data);
+        console.log("sucess: " + response.data.success);
+      } catch (error) {
+        console.log(error.response.body);
       }
+    })();
 
-    const req = https.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-      
-        res.on('data', d => {
-          console.log(d)
-        })
-      })
-      
-    req.on('error', error => {
-        console.error(error)
-    })
-
-    req.end();
 
     console.log('End of web request');
 
