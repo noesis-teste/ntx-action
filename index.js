73fa0a56bb9f030e63a138a4b8bcc2791df9ea7a
Luis Alho <luis.e.alho@noesis.pt>
2021-05-26 11:56:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,53 +3,135 @@ const github = require('@actions/github')
 
 const axios = require('axios');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 (async () => {
   try {
-    // `who-to-greet` input defined in action metadata file
+    // GET NTX URL from input
     const ntxURL = core.getInput('ntx-url');
     console.log(`Web request to ${ntxURL}`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("status", time);
+    //const time = (new Date()).toTimeString();
+    //core.setOutput("status", time);
+
+    
+    const username = 'alho';
+    const password = 'admin';
+    var basicAuth = 'Basic ' + Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
+
+    //http://localhost:8084/NTX/ntxWebService?LinkID=88&TypeID=1&MachineID=71&RepositoryID=3&ProjectID=10100&Version=10100&Cycle=PDT-2396&UserId=14&ServerURL=3&EnvironmentId=5
+
+    // http://localhost:8084/NTX/ntxWebService?LinkID=89&TypeID=1&MachineID=71&RepositoryID=2&ProjectID=3dd5ff83-ecdc-465a-acea-fc7847ba5878&Version=3292&Cycle=3293&UserId=14&ServerURL=5&EnvironmentId=5&Collection=DefaultCollection&Token=3ztvxjlqiguamhcl7y4eo4tfvx2gtqiseqglwxhyowyuzcm3f32a
+  //  try {
+      const response = await axios.get('http://172.30.160.1:8084/NTX/ntxWebService?LinkID=89&TypeID=1&MachineID=71&RepositoryID=2&ProjectID=3dd5ff83-ecdc-465a-acea-fc7847ba5878&Version=3292&Cycle=3293&UserId=14&ServerURL=5&EnvironmentId=5&Collection=DefaultCollection&Token=3ztvxjlqiguamhcl7y4eo4tfvx2gtqiseqglwxhyowyuzcm3f32a', {
+        auth: {
+          username: 'alho',
+          password: 'admin'
+        }
+      })
 
 
+      console.log("Response: ", response.data);
+      const { id } = response.data;
 
-    try {
-      const response = await axios.get('https://reqbin.com/echo/get/json')
-      console.log("ntx-url: " + response.data);
-      console.log("sucess: " + response.data.success);
+      if (id == -1){
+        console.log("Error getting execution ID");
+        core.setFailed("Error getting execution ID");
+        return;
+      }
+      
+/*
     } catch (error) {
       console.log(error.response.body);
       core.setFailed("Error getting execution ID");
       return;
     }
+*/
+    console.log("Success: " + id);
 
-
+    let isComplete = false;
     //Start looping until response comes
     do{
 
       //Wait for X seconds
-      await sleep(20 * 1000);
-
+      console.log("Wait for 20 seconds");
+      try{
+        await sleep(10 * 1000);
+      }catch{
+        console.log("Ending wait");
+      }
+      
       //Ask for status of execution
-      try {
-        const response = await axios.get('https://reqbin.com/echo/get/json')
-        console.log("ntx-url: " + response.data);
-        console.log("sucess: " + response.data.success);
-      } catch (error) {
-        console.log(error.response.body);
-        core.setFailed("Error getting execution ID");
-        return;
+      const response = await axios.get(`http://172.30.160.1:8084/NTX/ntxWebService?GetStatusId=${id}`, {
+        auth: {
+          username: 'alho',
+          password: 'admin'
+        }
+      })
+      
+      console.log("Response: ", response.data);
+      const code = response.data.status_code;
+
+      console.log("Code: " + code);
+      switch(code){
+
+        case 1:
+          isComplete = false;
+          core.info("Execution created")
+          break;
+
+        case 2: 
+          isComplete = false;
+          core.info("Execution running")
+          break;
+        case 3:
+          isComplete = true
+          core.info("All tests passed")
+          break;
+        case 4 :
+          isComplete = true;
+          core.setFailed("Some tests have failed");
+          break;
+        case -2 :
+          isComplete = true;
+          core.setFailed("The execution ID is invalid");
+          break;
+        case -3 :
+          isComplete = true;
+          core.setFailed("The project, version or cycle doesn't exist");
+          break;
+        case -4 :
+          isComplete = true;
+          core.setFailed("Machine doesn't exist");
+          break;
+        case -5 :
+          isComplete = true;
+          core.setFailed("The cycle doesn´t have tests");
+          break;
+        case -6 :
+          isComplete = true;
+          core.setFailed("Unknown error");
+          break;
+        case -7 :
+          isComplete = true;
+          core.setFailed("Device doesn't exist");
+          break;
+
+        default:
+          isComplete = true;
+          core.setFailed("Unknown code status");
+          break;
+
       }
 
+      
+      console.log("isComplete: " + isComplete);
 
+    }while( !isComplete );
 
-    }while(response.data.success != "true" || response.data.success != true)
+    console.log('End of action');
 
-    console.log('End of web request');
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
